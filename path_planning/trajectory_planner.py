42372caeba19a2cb86232e90c1c85a22e1a2c985
ADJohnson314 <adj314@mit.edu>
2024-04-15 13:21:39 +0000

    Cleaned up utils.py file with pylint, prepared trajectory_planner.py to display traj's

--- path_planning/trajectory_planner.py
+++ path_planning/trajectory_planner.py
@@ -1,18 +1,30 @@
 import rclpy
 from rclpy.node import Node
+import os
 
 assert rclpy
 from geometry_msgs.msg import PoseWithCovarianceStamped, PoseStamped, PoseArray
 from nav_msgs.msg import OccupancyGrid
+from visualization_msgs.msg import Marker
 from .utils import LineTrajectory
 
 
+# TODO:
+#   Choose an algorithm to plan trajectories with (probably A*)
+#   Write/comment code for selecting one of several algo's (in case others are written)
+#   Figure out how the OccupancyGrid message can be used to determine what points are traversable
+#   Write the code for the selected algorithm, and make sure that it works
+#   Clean up the code w/ pylint and black
+
 class PathPlan(Node):
-    """ Listens for goal pose published by RViz and uses it to plan a path from
-    current car pose.
+    """
+    Listens for goal pose published by RViz and uses it to plan a path from current car pose.
     """
 
     def __init__(self):
+        """
+        Initialize pubs and subs, and create an instance of the LineTrajectory class.
+        """
         super().__init__("trajectory_planner")
         self.declare_parameter('odom_topic', "default")
         self.declare_parameter('map_topic', "default")
@@ -48,20 +60,59 @@ class PathPlan(Node):
             10
         )
 
+        self.traj_sub_start = self.create_subscription(
+            Marker,
+            "/planned_trajectory/start_point",
+            self.test_path,
+            10
+        )
+
+        self.traj_sub_start = self.create_subscription(
+            Marker,
+            "/planned_trajectory/end_pose",
+            self.test_path,
+            10
+        )
+
+        self.traj_sub = self.create_subscription(
+            Marker,
+            "/planned_trajectory/path",
+            self.test_path,
+            10
+        )
+
         self.trajectory = LineTrajectory(node=self, viz_namespace="/planned_trajectory")
 
     def map_cb(self, msg):
-        raise NotImplementedError
+        """
+        Upon receiving a map message, display the data in the simulation, then...
+        """
+        self.get_logger().info("Recieved map data!")
+        self.plan_path()
 
     def pose_cb(self, pose):
-        raise NotImplementedError
+        """
+        ...
+        """
+        self.get_logger().info("Received initlal pose data!")
 
     def goal_cb(self, msg):
-        raise NotImplementedError
+        """
+        ...
+        """
+        self.get_logger().info("Received goal pose data!")
 
-    def plan_path(self, start_point, end_point, map):
+    def plan_path(self, start_point=None, end_point=None, map=None):
+        """
+        ...
+        """
+        directory = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+        self.trajectory.load(directory + "/example_trajectories/trajectory_1.traj")
         self.traj_pub.publish(self.trajectory.toPoseArray())
         self.trajectory.publish_viz()
+
+    def test_path(self, msg):
+        self.get_logger().info(f"Recieved trajectory data! {msg}")
 
 
 def main(args=None):
--- path_planning/utils.py
+++ path_planning/utils.py
@@ -1,23 +1,28 @@
+import json
+import os
+from typing import List, Tuple
 import rclpy
-
 import numpy as np
+
 from visualization_msgs.msg import Marker
 from geometry_msgs.msg import Pose, PoseArray, Point
 from std_msgs.msg import Header
-import os
-from typing import List, Tuple
-import json
 
 EPSILON = 0.00000000001
 
-''' These data structures can be used in the search function
-'''
-
+"""
+These data structures can be used in the search function.
+"""
 
 class LineTrajectory:
-    """ A class to wrap and work with piecewise linear trajectories. """
+    """
+    A class to wrap and work with piecewise linear trajectories.
+    """
 
     def __init__(self, node, viz_namespace=None):
+        """
+        Initialize attirbutes, and create publishers if viz_namespace is not None.
+        """
         self.points: List[Tuple[float, float]] = []
         self.distances = []
         self.has_acceleration = False
@@ -25,14 +30,23 @@ class LineTrajectory:
         self.viz_namespace = viz_namespace
         self.node = node
 
+        # viz_namespace is the namespace where the visualization topics should be located
         if viz_namespace:
             self.visualize = True
-            self.start_pub = self.node.create_publisher(Marker, viz_namespace + "/start_point", 1)
-            self.traj_pub = self.node.create_publisher(Marker, viz_namespace + "/path", 1)
-            self.end_pub = self.node.create_publisher(Marker, viz_namespace + "/end_pose", 1)
+            self.start_pub = self.node.create_publisher(
+                Marker, viz_namespace + "/start_point", 1
+            )
+            self.traj_pub = self.node.create_publisher(
+                Marker, viz_namespace + "/path", 1
+            )
+            self.end_pub = self.node.create_publisher(
+                Marker, viz_namespace + "/end_pose", 1
+            )
 
-    # compute the distances along the path for all path segments beyond those already computed
     def update_distances(self):
+        """
+        Compute the distances along the path for all path segments beyond those already computed.
+        """
         num_distances = len(self.distances)
         num_points = len(self.points)
 
@@ -46,57 +60,87 @@ class LineTrajectory:
                 self.distances.append(self.distances[i - 1] + np.linalg.norm(delta))
 
     def distance_to_end(self, t):
+        """
+        Given a trajectory point index t, compute the distance form this point to the end of the
+        path.
+        """
         if not len(self.points) == len(self.distances):
             print(
-                "WARNING: Different number of distances and points, this should never happen! Expect incorrect results. See LineTrajectory class.")
+                "WARNING: Different number of distances and points, this should never happen!"
+                + " Expect incorrect results. See LineTrajectory class."
+            )
         dat = self.distance_along_trajectory(t)
-        if dat == None:
+        if dat is None:
             return None
-        else:
-            return self.distances[-1] - dat
+
+        return self.distances[-1] - dat
 
     def distance_along_trajectory(self, t):
-        # compute distance along path
-        # ensure path boundaries are respected
+        """
+        Compute distance along path. Ensure path boundaries are respected. The input t is the index
+        of a point in the trajectory.
+        """
         if t < 0 or t > len(self.points) - 1.0:
             return None
         i = int(t)  # which segment
         t = t % 1.0  # how far along segment
         if t < EPSILON:
             return self.distances[i]
-        else:
-            return (1.0 - t) * self.distances[i] + t * self.distances[i + 1]
+
+        return (1.0 - t) * self.distances[i] + t * self.distances[i + 1]
 
     def addPoint(self, point: Tuple[float, float]) -> None:
+        """
+        Add a point to the trajectory. Points are represented as tuples containing the coordinates
+        of a location, of the form (x, y). Coordinates should be expressed in the map frame.
+        """
         print("adding point to trajectory:", point)
         self.points.append(point)
         self.update_distances()
         self.mark_dirty()
 
     def clear(self):
+        """
+        Remove all points and distances from the trajectory.
+        """
         self.points = []
         self.distances = []
         self.mark_dirty()
 
     def empty(self):
+        """
+        Return True if the trajectory has no points.
+        """
         return len(self.points) == 0
 
     def save(self, path):
+        """
+        Save a trajectory to a json file at the given file path. The path must be an absolute path.
+        """
         print("Saving trajectory to:", path)
         data = {}
         data["points"] = []
         for p in self.points:
             data["points"].append({"x": p[0], "y": p[1]})
-        with open(path, 'w') as outfile:
+        with open(path, "w") as outfile:
             json.dump(data, outfile)
 
     def mark_dirty(self):
+        """
+        Set this attribute to False.
+        """
         self.has_acceleration = False
 
     def dirty(self):
+        """
+        No acceleration makes a trajectory "dirty", apparently.
+        """
         return not self.has_acceleration
 
     def load(self, path):
+        """
+        Load a trajectory from a json file at the given absolute path.
+        """
         print("Loading trajectory:", path)
 
         # resolve all env variables in path
@@ -110,8 +154,10 @@ class LineTrajectory:
         print("Loaded:", len(self.points), "points")
         self.mark_dirty()
 
-    # build a trajectory class instance from a trajectory message
     def fromPoseArray(self, trajMsg):
+        """
+        Build a trajectory class instance from a trajectory message.
+        """
         for p in trajMsg.poses:
             self.points.append((p.position.x, p.position.y))
         self.update_distances()
@@ -119,6 +165,9 @@ class LineTrajectory:
         print("Loaded new trajectory with:", len(self.points), "points")
 
     def toPoseArray(self):
+        """
+        Convert a trajectory into a PoseArray object.
+        """
         traj = PoseArray()
         traj.header = self.make_header("/map")
         for i in range(len(self.points)):
@@ -130,6 +179,9 @@ class LineTrajectory:
         return traj
 
     def publish_start_point(self, duration=0.0, scale=0.1):
+        """
+        Publish the start point of the trajectory to the "/start_point" topic, as a Marker message.
+        """
         should_publish = len(self.points) > 0
         self.node.get_logger().info("Before Publishing start point")
         if self.visualize and self.start_pub.get_subscription_count() > 0:
@@ -161,6 +213,9 @@ class LineTrajectory:
             self.node.get_logger().info("Not publishing start point, no subscribers")
 
     def publish_end_point(self, duration=0.0):
+        """
+        Publish the end point of the trajectory to the "/end_pose" topic, as a Marker message.
+        """
         should_publish = len(self.points) > 1
         if self.visualize and self.end_pub.get_subscription_count() > 0:
             marker = Marker()
@@ -190,6 +245,9 @@ class LineTrajectory:
             print("Not publishing end point, no subscribers")
 
     def publish_trajectory(self, duration=0.0):
+        """
+        Publish the entire trajectory to "/path", as a Marker message.
+        """
         should_publish = len(self.points) > 1
         if self.visualize and self.traj_pub.get_subscription_count() > 0:
             self.node.get_logger().info("Publishing trajectory")
@@ -216,11 +274,14 @@ class LineTrajectory:
                 # delete
                 marker.action = marker.DELETE
             self.traj_pub.publish(marker)
-            print('publishing traj')
+            print("publishing traj")
         elif self.traj_pub.get_subscription_count() == 0:
             print("Not publishing trajectory, no subscribers")
 
     def publish_viz(self, duration=0):
+        """
+        Publish everything about the trajectory to be visualized.
+        """
         if not self.visualize:
             print("Cannot visualize path, not initialized with visualization enabled")
             return
@@ -229,7 +290,10 @@ class LineTrajectory:
         self.publish_end_point(duration=duration)
 
     def make_header(self, frame_id, stamp=None):
-        if stamp == None:
+        """
+        Make a header for a message holding trajectory data.
+        """
+        if stamp is None:
             stamp = self.node.get_clock().now().to_msg()
         header = Header()
         header.stamp = stamp
